6b6b4d32b5af4b388ed9c5be53903747205b9b79
Paul Abawe <paul.abawe@ttiinc.com>
2024-10-24 00:18:07 +0000

    Oct 23, 2024, 7:18 PM

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,5 @@
 import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import ("@nomicfoundation/hardhat-toolbox");
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -7,7 +7,8 @@ const networkUrl = process.env.PROVIDER_URL;
 const privateKey = process.env.PRIVATE_KEY;
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  // solidity: "0.8.19",
+  solidity: "0.8.20", // 0.8.20 and later breaks test for some reason
   networks: {
     goerli: {
       url: networkUrl,
